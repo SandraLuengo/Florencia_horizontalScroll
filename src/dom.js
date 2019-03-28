71672b1121bd5b68bdd3daf669289ce7a56a8809
SandraLuengo <you@example.com>
2019-03-28 15:48:13 +0100

    scroll horizontal

--- src/dom.js
+++ src/dom.js
@@ -1,46 +1,70 @@
 window.onload = () => {
+  function repeatOften() {
+    var obj=mover();
+    if (obj.true) {
+      if(obj.mov==='iz'){
+        window.scrollBy(-10, 0)
+      }else if(obj.mov==='de'){
+        window.scrollBy(10, 0)
+      }
+    }
+    requestAnimationFrame(repeatOften);
+  }
+  requestAnimationFrame(repeatOften);
 
   let main = document.querySelector(".App");
   let xVal = 0;
   let yVal = 0;
-  let client=0;
-  main.addEventListener("mousemove", function (e) {
+  let client = 0;
+  let scrollLeft = 0;
+  let width = main.clientWidth;
 
-    let width = main.clientWidth;
+  main.addEventListener("mousemove", (e) => {
+    mover();
     let heigth = main.clientHeight;
-    client=e.clientX;
+    client = e.clientX;
     xVal = (e.clientX + document.documentElement.scrollLeft) / width;
     yVal = e.clientY / heigth;
-    // console.log(width, e.clientX, xVal);
-    // console.log(document.documentElement.scrollLeft)
-    console.log(xVal)
 
-    if (e.clientX >= 0 && e.clientX < width * 0.01) {
-      console.log('izquerda true')
-      // izquerda = true;
-
-    } else {
-      console.log('izquerda false')
-      // izquerda = false;
-    }
-
-    if (e.clientX > width * 0.9 && e.clientX <= width) {
-      console.log('derecha')
-      // window.scrollBy(0, 10);
-    }
 
     main.style.setProperty("--mouse-x", xVal);
     main.style.setProperty("--mouse-y", yVal);
 
   });
 
-  window.addEventListener("scroll", function (e) {
-    console.log('scroll')
-    console.log(xVal)
-    main.style.setProperty("--mouse-x",(( client + document.documentElement.scrollLeft) /  main.clientWidth));
+  window.addEventListener("scroll", (e) => {
+    mover();
+    scrollLeft = document.documentElement.scrollLeft;
+    main.style.setProperty("--mouse-x", ((client + scrollLeft) / main.clientWidth));
     main.style.setProperty("--mouse-y", yVal);
 
   });
+
+  let mover = () => {
+
+    if (client < width * 0.05) {
+
+      if ((client >= 0 && client < width * 0.04) && scrollLeft > 15) {
+        
+        return {true:true,mov:'iz'};
+      } else {
+     
+        return {true:false,mov:'iz'};;
+      }
+
+    } else {
+      console.log(client,width*0.3,width)
+      if (client > width * 0.5 && client <= width) {
+        console.log('derecha true')
+        return {true:true,mov:'de'}
+      }else{
+        console.log('derecha false')
+        return{true:false,mov:'iz'};
+      }
+    }
+
+
+  }
 
 
 }
